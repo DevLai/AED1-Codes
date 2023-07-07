194c741bdfb198f29cc57d8f9bb85486eb4f4e14
Laiane <137008085+DevLai@users.noreply.github.com>
2023-07-07 14:30:15 -0300

    Add files via upload

---
+++ Leetcode/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*typedef struct shortestcompletingword{
+char licenseplate;
+char words;
+} * shortest; */
+
+/*shortestcompletingword(){
+
+}*/
+
+
+  int main(){
+      int a;
+      int b;
+      int r;
+
+      printf("Entre com o primeiro valor: ");
+      scanf("%d", &a);
+      printf("Entre com o segundo valor: ");
+      scanf("%d", &b);
+
+      r = a + b;
+
+      printf("Resultado da soma: %d\n", r);
+
+
+
+    return 0;
+}
