705055cadc13494c658457f4d8c9eed069aaeea3
Laiane <137008085+DevLai@users.noreply.github.com>
2023-09-17 23:04:17 -0300

    Add files via upload

--- AvPratica2/main.c
+++ AvPratica2/main.c
@@ -1,60 +1,68 @@
-int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
-    // struct pra uma hashtable que vai aarmazenar os valores e o count
-    struct hashtable {
-        int valor;
-        int count;
-    };
+#include <stdio.h>
+#include <stdlib.h>
 
-     // Função de comparação pro qsort que tbm vai ordenar de forma decrescente
-    int compararhash(const void *a, const void *b) {
-        return ((struct hashtable *)b)->count - ((struct hashtable *)a)->count;
-    }
+// Definição da estrutura TreeNode_t
+struct TreeNode_t {
+    int val;
+    struct TreeNode_t* esquerda;
+    struct TreeNode_t* direita;
+};
 
+/*
+===============================================================================
+findDif
+ Função auxiliar para encontrar o segundo valor mínimo diferente de um dado valor
+===============================================================================
+*/
+int findDif_r( struct TreeNode_t* root, int val ) {
+    if ( root == NULL )
+        return -1;
 
-    // Função de comparação pro qsort
-    int comparar(const void *a, const void *b) {
-        return *(const int *)a - *(const int *)b;
-    }
+    if ( root->val != val )
+        return root->val;
 
+    int leftValue = findDif_r( root->esquerda, val );
+    int rightValue = findDif_r( root->direita, val );
 
-    // malloc de memória pro array de saída
-    int *array = malloc(k * sizeof(int));
+    if ( leftValue == -1 )
+        return rightValue;
 
-    // struct de hashtable que vai contar a frequência dos valores
-    struct hashtable hash[numsSize];
-    memset(hash, 0, sizeof(struct hashtable) * numsSize); // eu tentei fazer essa parte com sizeof,
-    //mas não estava conseguindo e o código sempre retornava erro, então pesquisei uma outra alternativa e descobri o memset
+    if ( rightValue == -1 )
+        return leftValue;
 
-    int i;
-    int count = 1;
-    *returnSize = k;
-
-   
-
-    // Contar a frequência dos valores e armazenar na hashtable
-    for (i = 1; i < numsSize; i++) {
-        if (hash[count - 1].valor == nums[i]) {
-            hash[count - 1].count++;
-        } else {
-            count++;
-            hash[count - 1].valor = nums[i];
-            hash[count - 1].count++;
-        }
-    }
-     // Ordenar o array de entrada
-    qsort(nums, numsSize, sizeof(int), comparar);
-
-    hash[0].valor = nums[0];
-    hash[0].count++;
-
-    // Ordenar a hashtable pela contagem em ordem decrescente
-    qsort(hash, count, sizeof(hash[0]), compararhash);
-
-    // Preencher o array de saída com os k elementos mais frequentes
-    for (i = 0; i < k; i++) {
-        array[i] = hash[i].valor;
-    }
-
-    return array;
+    return fmin( leftValue, rightValue );
 }
 
+/*
+===============================================================================
+findSecondMinimumValue
+Função principal para encontrar o segundo valor mínimo diferente
+===============================================================================
+*/
+
+int findSecondMinimumValue( struct TreeNode_t* root ) {
+    // Vai chamar a função findDif_r com a raiz da árvore e o valor da raiz
+    return findDif_r(root, root->val);
+}
+
+int main() {
+    // Criar a árvore de exemplo: [2, 2, 5, null, null, 5, 7]
+    struct TreeNode_t* root = ( struct TreeNode_t*)malloc(sizeof( struct TreeNode_t ) );
+    root->val = 2;
+    root->esquerda = ( struct TreeNode_t* )malloc( sizeof( struct TreeNode_t ) );
+    root->esquerda->val = 2;
+    root->esquerda->esquerda = NULL;
+    root->esquerda->direita = NULL;
+    root->direita = ( struct TreeNode_t* )malloc( sizeof( struct TreeNode_t ) );
+    root->direita->val = 5;
+    root->direita->esquerda = NULL;
+    root->direita->direita = ( struct TreeNode_t* )malloc( sizeof( struct TreeNode_t ) );
+    root->direita->direita->val = 7;
+    root->direita->direita->esquerda = NULL;
+    root->direita->direita->direita = NULL;
+
+    int result = findSecondMinimumValue( root );
+    printf( "Second minimum value: %d\n", result ); // output vai ser 5
+
+    return 0;
+}
